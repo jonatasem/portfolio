ce5ccc67ab1ffb624e9347b8f319ec74d89a5d0d
jonatasem <jonatas.em20@gmail.com>
2024-11-15 16:06:41 -0300

    adicionando novo projeto

--- src/api/ProjectsData.js
+++ src/api/ProjectsData.js
@@ -1,11 +1,13 @@
-import eCommerce from "../assets/img/projects/e-commerce.png";
+import imgCommerce from "../assets/img/projects/e-commerce.png";
 import imgMovies from "../assets/img/projects/Movies.png";
 import imgRestaurant from "../assets/img/projects/restaurant.png";
+import imgViagens from '../assets/img/projects/viagens.png';
+import imgGraphic from '../assets/img/projects/graphic.png';
 
 const ProjectsData = [
     {
         id: 0,
-        nome: "Restaurant",
+        nome: "Menu Restaurante",
         img: imgRestaurant,
         description:
         "Desenvolvido com React.tsx, utilizando redux e axios para requisição.",
@@ -16,7 +18,7 @@ const ProjectsData = [
     },
     {
         id: 1,
-        nome: "Movies",
+        nome: "Filmes",
         img: imgMovies,
         description:
             "Desenvolvido com React Vite, utilizando SASS.",
@@ -28,12 +30,34 @@ const ProjectsData = [
     {
         id: 2,
         nome: "E-commerce",
-        img: eCommerce,
+        img: imgCommerce,
         description:
             "Desenvolvido com React Vite, utilizando redux para gerenciamento de estado e sass.",
         link: {
             live: "https://jonatasem.github.io/e-commerce/",
             github: "https://github.com/jonatasem/e-commerce",
+        },
+    },
+    {
+        id: 3,
+        nome: "Viagens",
+        img: imgViagens,
+        description:
+            "Desenvolvido com HTML, CSS e JavaScript.",
+        link: {
+            live: "https://jonatasem.github.io/viagens/",
+            github: "https://github.com/jonatasem/viagens",
+        },
+    },
+    {
+        id: 4,
+        nome: "Grafico de Avaliação",
+        img: imgGraphic,
+        description:
+            "Desenvolvido com React.jsx e ApexCharts.js",
+        link: {
+            live: "https://jonatasem.github.io/graphic/",
+            github: "https://github.com/jonatasem/graphic",
         },
     }
 ];
